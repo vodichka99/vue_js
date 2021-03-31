2fd5ceeacee0347a1c3f50ddfef0b2be02817746
Eugene <kimevgenijkimpum@gmail.com>
2021-03-31 18:05:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const App = {
+    data(){
+        return{
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: []
+        }
+    },
+    methods: {
+        addNewNote(){
+            if(this.inputValue !== ''){
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item){
+            return item.toUpperCase()
+        },
+        removeNote(idx){
+            this.notes.splice(idx, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed(){
+            console.log('doubleCountComputed');
+            return this.notes.length*2
+        },
+    },
+    watch: {
+        inputValue(value){
+            if(value.length > 10){
+                this.inputValue = ''
+            }
+            console.log('input Value',value);
+        }
+    }
+}
+
+
+
+Vue.createApp(App).mount('#app')
